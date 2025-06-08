b26f40c3ad613732ca4dd76945bb2c3007547fd2
Quayyum <quayyumariyo@gmail.com>
2025-06-08 20:22:24 +0100

    Delete Tic-Tac-Toe directory

--- Tic-Tac-Toe/src/player.py
+++
@@ -1,7 +0,0 @@
-from src.cell_state import CellState
-
-class Player:
-    def __init__(self, name: str, symbol: CellState, is_ai: bool = False):
-        self.name = name
-        self.symbol = symbol
-        self.is_ai = is_ai
--- Tic-Tac-Toe/src/utils.py
+++
@@ -1,9 +0,0 @@
-def get_valid_input(prompt: str, min_val: int, max_val: int) -> int:
-    while True:
-        try:
-            value = int(input(prompt))
-            if min_val <= value <= max_val:
-                return value
-            print(f"Please enter a number between {min_val} and {max_val}.")
-        except ValueError:
-            print("Please enter a valid number.")
