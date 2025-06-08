9b4e863e583879a13c91e346483cecd22b87e4a9
Quayyum-a <quayyumariyo@gmail.com>
2025-06-08 22:38:06 +0100

    added functionality

--- app/presentation/order_controller.py
+++ app/presentation/order_controller.py
@@ -17,7 +17,7 @@ def place_order():
 
 @bp.route('', methods=['GET'])
 @jwt_required()
-      def get_orders():
+def get_orders():
           identity = get_jwt_identity()
           orders = OrderService().get_user_orders(identity['id'])
           return jsonify([{'id': o.id, 'product_id': o.product_id, 'quantity': o.quantity, 'status': o.status} for o in orders]), 200
