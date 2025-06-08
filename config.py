0b99e2247ed68d1dfcc6d4af4cea10df9b9bc49c
Quayyum-a <quayyumariyo@gmail.com>
2025-06-08 22:58:51 +0100

    added functionality

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+pymysql://user:password@localhost/ecommerce')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+pymysql://root:Mavixess@localhost/e_commerce_py')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your-secret-key')
