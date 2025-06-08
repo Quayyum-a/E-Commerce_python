033078224fb8587aeb02342bb8daf0fa1fd7fd91
Quayyum-a <quayyumariyo@gmail.com>
2025-06-08 21:32:05 +0100

    setup

--- app/__init__.py
+++ app/__init__.py
@@ -7,14 +7,14 @@ from config import Config
 db = SQLAlchemy()
 migrate = Migrate()
 
-   def create_app():
-       app = Flask(__name__)
-       app.config.from_object(Config)
-       db.init_app(app)
-       migrate.init_app(app, db)
-       JWTManager(app)
-       from app.presentation import auth_controller, product_controller, order_controller
-       app.register_blueprint(auth_controller.bp)
-       app.register_blueprint(product_controller.bp)
-       app.register_blueprint(order_controller.bp)
-       return app
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    JWTManager(app)
+    from app.presentation import auth_controller, product_controller, order_controller
+    app.register_blueprint(auth_controller.bp)
+    app.register_blueprint(product_controller.bp)
+    app.register_blueprint(order_controller.bp)
+    return app
