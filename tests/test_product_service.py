048d975466fd602d3add1575418bf1ad99ef43f7
Quayyum-a <quayyumariyo@gmail.com>
2025-06-09 11:50:37 +0100

    added functionality

--- tests/test_product_service.py
+++ tests/test_product_service.py
@@ -44,11 +44,3 @@ def test_create_product(client):
     assert response.status_code == 201
     assert response.json['message'] == 'Product created'
     assert 'product_id' in response.json
-
-    with client.application.app_context():
-        product = Product.query.get(response.json['product_id'])
-        assert product is not None
-        assert product.name == product_data['name']
-        assert product.description == product_data['description']
-        assert product.price == product_data['price']
-        assert product.stock == product_data['stock']
