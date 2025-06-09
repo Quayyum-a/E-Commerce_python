af4b9d21abecd5369724533857dcf1f29bff035c
Quayyum-a <quayyumariyo@gmail.com>
2025-06-09 08:34:21 +0100

    added functionality

--- app/domain/order.py
+++ app/domain/order.py
@@ -1,6 +1,7 @@
 from app import db
 
 class Order(db.Model):
+    __tablename__ = 'order'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
