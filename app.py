dd83b06378a7afe922b2d60833d0b84e3c1f72f9
Quayyum-a <quayyumariyo@gmail.com>
2025-06-10 07:17:24 +0100

    added functionality

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 from app.presentation.auth_controller import bp as auth_bp
 from app.presentation.product_controller import bp as product_bp
 from app.presentation.order_controller import bp as order_bp
 from app.infrastructure.database import init_db
 
 app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'your-secret-key'  # Change this to a strong secret in production
+jwt = JWTManager(app)
+CORS(app)
 
 # Register blueprints
 app.register_blueprint(auth_bp)
@@ -21,4 +26,4 @@ def hello_world():  # put application's code here
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
