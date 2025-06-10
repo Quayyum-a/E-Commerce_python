fc30c7a4e5b831dcf99f0b78f247cb610b3613f4
Quayyum-a <quayyumariyo@gmail.com>
2025-06-10 08:35:27 +0100

    added functionality

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 from config import Config
 
 db = SQLAlchemy()
@@ -10,11 +11,25 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    
+    # Configure CORS
+    CORS(app, 
+         resources={
+             r"/api/*": {
+                 "origins": ["http://127.0.0.1:5500", "http://localhost:5500"],
+                 "supports_credentials": True,
+                 "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+                 "allow_headers": ["Content-Type", "Authorization"]
+             }
+         })
+    
     db.init_app(app)
     migrate.init_app(app, db)
     JWTManager(app)
+    
     from app.presentation import auth_controller, product_controller, order_controller
-    app.register_blueprint(auth_controller.bp)
-    app.register_blueprint(product_controller.bp)
-    app.register_blueprint(order_controller.bp)
+    app.register_blueprint(auth_controller.bp, url_prefix='/api')
+    app.register_blueprint(product_controller.bp, url_prefix='/api')
+    app.register_blueprint(order_controller.bp, url_prefix='/api')
+    
     return app
