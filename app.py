ad1656c8304d4ba60dd4ae1a75796510e1539455
Quayyum-a <quayyumariyo@gmail.com>
2025-06-10 08:07:13 +0100

    added functionality

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from app import create_app
+from flask_cors import CORS
 
 app = create_app()
+CORS(app, origins=["http://127.0.0.1:5500", "http://localhost:5500"], supports_credentials=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
