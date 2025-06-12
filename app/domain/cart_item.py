d364c1c23c7c3f93daf4a084d11075f82a959613
Quayyum-a <quayyumariyo@gmail.com>
2025-06-12 12:04:15 +0100

    added functionality

--- app/domain/cart_item.py
+++ app/domain/cart_item.py
@@ -1,7 +1,13 @@
-class CartItem:
-    def __init__(self, id: int, cart_id: int, product_id: int, quantity: int):
-        self.id = id
+from app import db
+
+class CartItem(db.Model):
+    __tablename__ = 'cart_items'
+    id = db.Column(db.Integer, primary_key=True)
+    cart_id = db.Column(db.Integer, db.ForeignKey('carts.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, cart_id, product_id, quantity):
         self.cart_id = cart_id
         self.product_id = product_id
         self.quantity = quantity
-
